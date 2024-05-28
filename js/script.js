0fd0a1858ba7b65cddedbff8ede03132b6f17849
Chritie MPASSI <christmp19@gmail.com>
2024-05-28 07:12:18 +0100

    Fist commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+ 
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelector('.home').onmousemove = (e) =>{
+
+    let x = (window.innerWidth - e.pageX * 2) / 90;
+    let y = (window.innerHeight - e.pageY * 2) / 90;
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px) translateY(${x}px)`;
+}
+
+document.querySelector('.home').onmouseleave = () =>{
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
+}   
